f6003647e6c1acc2320d08c0ff0b4655f2ae7351
Adam Azmil <muhd.adam1@gmail.com>
2021-08-12 15:33:30 -0500

    Piano VR

---
+++ Assets/KeyPressB.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class KeyPressB : MonoBehaviour
+{
+    //public Animator animator; 
+    // Start is called before the first frame update
+    void Start()
+    {
+    //animator= GetComponent<Animator>();
+    //animation=GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //public AudioSource playSound;
+    
+     public AudioClip clip;
+    private void OnTriggerEnter(Collider other){
+        
+                if (GetComponent<Animation>()!=null){
+                    Debug.Log("playing");
+                    GetComponent<Animation>().Play("KeyPressB");
+                    AudioSource.PlayClipAtPoint(clip, new Vector3(-0.7f,3.4f,1.1f));
+        }
+
+    }
+
+}
---
+++ Assets/OpenCover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCover : MonoBehaviour
+{
+    //public Animator animator; 
+    // Start is called before the first frame update
+    void Start()
+    {
+    //animator= GetComponent<Animator>();
+    //animation=GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other){
+        
+                if (GetComponent<Animation>()!=null){
+                    Debug.Log("playing");
+                    GetComponent<Animation>().Play("openCover");
+        }
+
+    }
+
+}
